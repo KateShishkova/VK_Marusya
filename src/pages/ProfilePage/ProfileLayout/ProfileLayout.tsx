8cfa89d7f82bb4a84bdb23ea0db89a7320a2b192
KateShishkova <ekaterina.shishkova6@gmail.com>
2026-04-13 02:38:50 +0400

    feat(ProfileLayout): add ProfileLayout component

---
+++ src/pages/ProfilePage/ProfileLayout/ProfileLayout.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import { Outlet } from "react-router-dom";
+import { ProfileNavList } from "@components/Profile/ProfileNavList";
+import styles from "./ProfileLayout.module.scss";
+
+export const ProfileLayout = () => {
+  return (
+    <>
+      <section className={clsx("section", styles.section)}>
+        <div className="container">
+          <div className={styles.section__wrapper}>
+            <h2 className={styles.section__title}>Мой аккаунт</h2>
+            <ProfileNavList />
+          </div>
+        </div>
+      </section>
+
+      <Outlet />
+    </>
+  );
+};
