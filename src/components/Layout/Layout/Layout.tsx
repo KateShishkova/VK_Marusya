62c51f6391e60a31933bfcedd295e74affd22244
KateShishkova <ekaterina.shishkova6@gmail.com>
2026-04-17 03:03:30 +0400

    feat(router): add router loader to layout

--- src/components/Layout/Layout/Layout.tsx
+++ src/components/Layout/Layout/Layout.tsx
@@ -1,7 +1,21 @@
+import clsx from "clsx";
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+
+import { Loader } from "@components/UI/Loader";
 import { usePageTitle } from "@hooks/usePageTitle";
+
 import { Header } from "../Header";
 import { Footer } from "../Footer";
+import styles from "./Layout.module.scss";
+
+const routerLoader = (
+  <section className={styles.section}>
+    <div className={clsx("container", styles.section__container)}>
+      <Loader />
+    </div>
+  </section>
+);
 
 export const Layout = () => {
   const pageTitle = usePageTitle();
@@ -11,7 +25,9 @@ export const Layout = () => {
       <Header />
       <main>
         <h1 className="visually-hidden">{pageTitle}</h1>
-        <Outlet />
+        <Suspense fallback={routerLoader}>
+          <Outlet />
+        </Suspense>
       </main>
       <Footer />
     </>
